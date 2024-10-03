36d25eca26d17813749336ed24a274709d6e48e5
Christian David Jasmin <kurosawataki84@gmail.com>
2024-10-03 11:35:50 +0800

    added modules

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,11 @@ import express from "express";
 import cors from "cors";
 import routes from "../routes/crud.mjs";
 import { json } from "express";
+import { createClient } from "@supabase/supabase-js";
 const app = express();
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY ;
+const supabase = createClient(supabaseUrl, supabaseKey);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`LISTENING TO PORT ${PORT}`)
