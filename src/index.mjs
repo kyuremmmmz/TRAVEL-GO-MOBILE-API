ad501c51cd7c3a7d26eb7ba4c7260f738256e305
Christian David Jasmin <kurosawataki84@gmail.com>
2024-10-03 11:37:54 +0800

    added supabase client

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,7 @@ import routes from "../routes/crud.mjs";
 import { json } from "express";
 import { createClient } from "@supabase/supabase-js";
 const app = express();
+require('dotenv').config;
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY ;
 const supabase = createClient(supabaseUrl, supabaseKey);
